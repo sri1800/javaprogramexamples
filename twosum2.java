9010ec01c5df7ebf3a6051bf127a326b9ca9b1a5
sri1800 <chittelasrihari18001@gmail.com>
2024-12-13 19:33:44 +0530

    by sorting

---
+++ twosum2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class twosum2
+{
+	public static String twosum(int n,int []num,int target)
+	{
+		Arrays.sort(num);
+		int left=0,right=n-1;
+		
+		while (left<right){
+		int sum=num[left]+num[right];
+		if (sum==target)
+		{
+		return "yes";
+		}else if (sum<target) left++;
+		else right--;
+		}
+		return "no";
+	}
+	
+	public static void main (String args[])
+	{
+	int n=5;
+	int []num={2,5,8,6,11};
+	int target=14;
+	String ans=twosum(n,num,target);
+	System.out.println(ans);
+	}
+}
