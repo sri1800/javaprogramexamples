16d5f412daba8ce1c9dc99578ed1a7945f37b2b2
sri1800 <chittelasrihari18001@gmail.com>
2024-12-13 13:29:51 +0530

    twosum(return indices)

--- twosum.java
+++ twosum.java
@@ -28,7 +28,7 @@ public class twosum
         int[] arr = {2, 6, 5, 8, 11};
         int target = 14;
         int[] ans = twoSum(n, arr, target);
-        System.out.println("This is the answer for variant 2:[ " + ans[0] +","+ ans[1]+"]");
+        System.out.println("This is the answer for variant 2::[ " + ans[0] +","+ ans[1]+"]");
     }
 
 }
