5f477d8b4a21e05b2981c7ab8b09b3ee9103ed9f
sri1800 <chittelasrihari18001@gmail.com>
2024-12-16 20:09:40 +0530

    array_problems

---
+++ arrayleader.java
@@ -0,0 +1,16 @@
+class arrayleader
+{
+    public static void main(String args[])
+    {
+        int[]a={1,5,8,6,4,1,8,4};
+        int n=a.length;
+        for(int i=0;i<n-1;i++)
+        {   
+            if(a[i]>=a[i+1])
+                {
+                    System.out.println(a[i]);
+                }
+            
+        }
+    }
+}
---
+++ kthelementofcombinedarray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.IntStream;
+class kthelementofcombinedarray
+{
+    public static void main(String args[]){
+        int a[]={5,4,6,8};
+        int b[]={1,3,6,9,11};
+        int k=5;
+        //int n=a.length+b.length;
+        //int[] result = new int[n];
+        int[] result = IntStream.concat(Arrays.stream(a), Arrays.stream(b)).toArray();
+        System.out.println(Arrays.toString(result));
+        Arrays.sort(result);
+        System.out.println("sorted" + Arrays.toString(result));
+        System.out.println("kthelement in combined sorted "+result[k]);
+    }
+}
